b2cd87cca658b32a703324106147ef546cb7b58f
Jack Huynh <huynhtrongnghia6102004@gmail.com>
2023-12-17 02:53:32 -0500

    first commit

---
+++ my-portfolio-website/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import Home from "./components/Home";
+import Navbar from "/MyPortfolioWebsite/my-portfolio-website/src/components/Navbar/Navbar"
+
+function App() {
+
+  return (
+      <div className="App">
+        <Navbar />
+        <Home />
+      </div>
+  );
+}
+
+export default App
---
+++ my-portfolio-website/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+// Navbar.js
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import homeLogo from "/MyPortfolioWebsite/my-portfolio-website/src/assets/icons/homeIcon.svg";
+import aboutLogo from "/MyPortfolioWebsite/my-portfolio-website/src/assets/icons/infoIcon.svg";
+import contactLogo from "/MyPortfolioWebsite/my-portfolio-website/src/assets/icons/mailIcon.svg";
+import "./Navbar.css";
+
+function Navbar() {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  const closeMenu = () => {
+    setMenuVisible(false);
+  };
+
+  return (
+    <div className="header">
+      <nav className="navbar">
+        <div className="hamburger-menu" onClick={toggleMenu}>
+          {menuVisible ? <FaTimes /> : <FaBars />}
+        </div>
+        <ul className={`nav-menu ${menuVisible ? 'show' : ''}`}>
+          <li>
+            <a href="/" className="home-link">
+              <img src={homeLogo} alt="homeLogo" className="homeLogo" />
+            </a>
+          </li>
+          <li>
+            <a href="/" className="about-link">
+              <img src={aboutLogo} alt="aboutLogo" className="aboutLogo" />
+            </a>
+          </li>
+          <li>
+            <a href="/" className="contact-link">
+              <img
+                src={contactLogo}
+                alt="contactLogo"
+                className="contactLogo"
+              />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
