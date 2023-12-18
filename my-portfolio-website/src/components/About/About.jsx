b6e878585eee15398b53ac7db15638d088b3938b
Jack Huynh <huynhtrongnghia6102004@gmail.com>
2023-12-18 01:31:55 -0500

    change stuff

---
+++ my-portfolio-website/src/components/About/About.jsx
@@ -0,0 +1,93 @@
+import './About.css'
+
+
+function About() {
+    return ( 
+        <>
+            <p>
+                hi I am
+            </p>
+            <p>
+                hi
+            </p>
+            <p>
+                hi
+            </p>
+            <p>
+                hi
+            </p>
+            <p>
+                hi
+            </p>
+            <p>
+                hi
+            </p>
+            <p>
+                hi there
+            </p>
+            <p>
+                hi
+            </p>
+            <p>
+                hi
+            </p>
+            <p>
+                hi
+            </p>
+            <p>
+                hi
+            </p>
+            <p>
+                hi
+            </p>
+            <p>
+                hi
+            </p>
+            <p>
+                hi
+            </p>
+            <p>
+                hi
+            </p>
+            <p>
+                hi
+            </p>
+            <p>
+                hi
+            </p>
+            <p>
+                hi
+            </p>
+            <p>
+                hi
+            </p>
+            <p>
+                hi
+            </p>
+            <p>
+                hi
+            </p>
+            <p>
+                hi
+            </p>
+            <p>
+                hi
+            </p>
+            <p>
+                hi
+            </p>
+
+            <p>
+                hi
+            </p><p>
+                hi
+            </p>
+
+            <p>
+                hi
+            </p>
+        </>
+    );
+}
+
+export default About;
