cc6c387eabcd758e5820a59cf1b41fc2007a9435
Jack Huynh <huynhtrongnghia6102004@gmail.com>
2023-12-18 06:25:00 -0500

    remove smooth scroll cuz not compatible with small screen need to do some research

--- my-portfolio-website/src/components/Navbar/Navbar.jsx
+++ my-portfolio-website/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-import { Link } from "react-scroll";
 import homeLogo from "/MyPortfolioWebsite/my-portfolio-website/src/assets/icons/homeIcon.svg";
 import aboutLogo from "/MyPortfolioWebsite/my-portfolio-website/src/assets/icons/infoIcon.svg";
 import contactLogo from "/MyPortfolioWebsite/my-portfolio-website/src/assets/icons/mailIcon.svg";
@@ -25,47 +24,23 @@ function Navbar() {
         </div>
         <ul className={`nav-menu ${menuVisible ? "show" : ""}`}>
           <li>
-            <Link
-              to="home-page"
-              spy={true}
-              smooth={true}
-              offset={-70} // Adjust this value as needed
-              duration={500}
-              className="home-link"
-              onClick={closeMenu}
-            >
+            <a href="/" className="home-link" onClick={closeMenu}>
               <img src={homeLogo} alt="homeLogo" className="homeLogo" />
-            </Link>
+            </a>
           </li>
           <li>
-            <Link
-              to="about-page"
-              spy={true}
-              smooth={true}
-              offset={-70} // Adjust this value as needed
-              duration={500}
-              className="about-link"
-              onClick={closeMenu}
-            >
+            <a href="#about-page" className="about-link" onClick={closeMenu}>
               <img src={aboutLogo} alt="aboutLogo" className="aboutLogo" />
-            </Link>
+            </a>
           </li>
           <li>
-            <Link
-              to="contact-section"
-              spy={true}
-              smooth={true}
-              offset={-70} // Adjust this value as needed
-              duration={500}
-              className="contact-link"
-              onClick={closeMenu}
-            >
+            <a href="/" className="contact-link" onClick={closeMenu}>
               <img
                 src={contactLogo}
                 alt="contactLogo"
                 className="contactLogo"
               />
-            </Link>
+            </a>
           </li>
         </ul>
       </nav>
