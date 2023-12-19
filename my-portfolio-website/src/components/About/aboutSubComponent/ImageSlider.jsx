7722e335f1e23c0e17b367e82931c84deb61f315
Jack Huynh <huynhtrongnghia6102004@gmail.com>
2023-12-19 08:57:35 -0500

    some minor changes

--- my-portfolio-website/src/components/About/aboutSubComponent/ImageSlider.jsx
+++ my-portfolio-website/src/components/About/aboutSubComponent/ImageSlider.jsx
@@ -4,7 +4,7 @@ import "./ImageSlider.css";
 
 function ImageSlider({ images }) {
   const [imageIndex, setImageIndex] = useState(0);
-  const [autoScroll, setAutoScroll] = useState(true);
+  const [autoScroll, setAutoScroll] = useState(false);
 
   function showNextImage() {
     setImageIndex((index) => (index === images.length - 1 ? 0 : index + 1));
