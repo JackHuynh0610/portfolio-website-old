d665e3e6ef274971ee9158e257ab85e2624f0021
Jack Huynh <huynhtrongnghia6102004@gmail.com>
2023-12-20 23:24:08 -0500

    console message

--- my-portfolio-website/src/main.jsx
+++ my-portfolio-website/src/main.jsx
@@ -5,6 +5,15 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {console.log("----------------------------------------------")}
+    {console.log("                                             ")}
+    {console.log("   ______      __      ________    |     /   ")}
+    {console.log("      |       /  \\    |            |   /    ")}
+    {console.log("      |      /----\\   |            |__/     ")}
+    {console.log("  |   |     /      \\  |            |   \\   ")}
+    {console.log("  |___|    /        \\ |_______     |    \\  ")}
+    {console.log("                                             ")}
+    {console.log("----------------------------------------------")}
     {console.log("Hi there!\nSeems like you want to see how this portfolio was made.\nYou can find the source code here: https://github.com/JackHuynh0610/portfolio-website\nBy the way, don't judge me too much, I made this while learning React + Vite. :-)")}
     {console.log("Contact me through my personal email: huynhtrongnghia6102004@gmail.com")}
     <App />
